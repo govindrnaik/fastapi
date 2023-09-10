7e395704975c4abe9145ab3c5a9bc7877a7b5a88
govind ratnakar naik <govindrnaik.trk@gmail.com>
2023-09-10 15:49:58 +0530

    added docker and environment variables

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import fastapi
+from fastapi import FastAPI
+import uvicorn
+
+from os import environ as env
 
 
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"data": f"Hello {env['MY_NAME_VARIABLE']}!!!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
